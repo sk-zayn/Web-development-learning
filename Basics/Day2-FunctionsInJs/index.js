1aaa64a5fba15c4a18423f3e09d1588cabcf245a
Zain Shaikh <skzain007995@gmail.com>
2025-12-16 10:25:39 +0530

    vowels count in string input

---
+++ Basics/Day2-FunctionsInJs/index.js
@@ -0,0 +1,13 @@
+// let vowels = ['a', 'e', 'i', 'o', 'u'];
+// let userInput = prompt('Enter a string here:');
+// vowelsCount = 0;
+// for(let i of vowels){
+//   for(let j of userInput){
+//     if(i == j){
+//       vowelsCount += 1
+//     }
+//   }
+// }
+// console.log(vowelsCount);
+
+
