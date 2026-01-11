7aef9132cfbfddbfecbf41d8cfe939efc8d3d47f
Zain Shaikh <skzain007995@gmail.com>
2026-01-11 13:31:35 +0530

    Add Tic Tac Toe game implementation with HTML, CSS, and JavaScript

---
+++ Basics/Day4-Tic Tac Toe/script.js
@@ -0,0 +1,19 @@
+// let student = {
+
+const { version } = require("react");
+
+//   fullName : 'Zain Shaikh',
+//   marks : 7.8,
+//   printMarks : function printMarks() {
+//     console.log(student.marks);
+//   },
+// };
+
+// console.log(student.fullName);
+// console.log(student.marks);
+// student.printMarks()
+
+
+
+console.log(process[l]);
+
